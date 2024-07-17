1fd3297e3e2a74e02e1a051d286589c0472ca612
emiyuel <108116222+emiyuel@users.noreply.github.com>
2024-07-17 15:20:56 +0900

    Title: 변수 출력하기 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240717/변수 출력하기 2/outputing-variables-2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int a = 3;
+    char b = 'C';
+    string c = "!.....!";
+
+    cout << b << c << a;
+    
+    return 0;
+}
