667bb42de69486f43f0e019a6ca179bed339d84e
emiyuel <108116222+emiyuel@users.noreply.github.com>
2024-07-25 01:36:39 +0900

    Title: 세 실수의 반올림, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240725/세 실수의 반올림/rounding-of-three-actual-numbers.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    double a, b, c;
+
+	cin >> a >> b >> c;
+
+    cout.precision(3);
+	cout << fixed << a << endl;
+    cout << b << endl;
+    cout << c << endl;
+
+	return 0;
+}
