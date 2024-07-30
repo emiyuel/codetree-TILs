6b1fc3b02a917446c1e386627dd930de656fd126
emiyuel <108116222+emiyuel@users.noreply.github.com>
2024-07-31 00:16:38 +0900

    Title: 시력 검사 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240731/시력 검사 2/eye-test-2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double sight;
+
+    cin >> sight;
+
+    if (sight >= 1.0)
+        cout << "High";
+    else if (sight >= 0.5)
+        cout << "Middle";
+    else
+        cout << "Low";
+        
+    return 0;
+}
