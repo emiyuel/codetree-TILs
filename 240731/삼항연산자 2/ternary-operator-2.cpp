feca30bda9fefebe28f34bafc39a706f5e57e772
emiyuel <108116222+emiyuel@users.noreply.github.com>
2024-07-31 00:11:20 +0900

    Title: 삼항연산자 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240731/삼항연산자 2/ternary-operator-2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+    char t_or_f;
+
+    cin >> a;
+
+    t_or_f = a == 1 ? 't' : 'f';
+
+    cout << t_or_f;
+    
+    return 0;
+}
