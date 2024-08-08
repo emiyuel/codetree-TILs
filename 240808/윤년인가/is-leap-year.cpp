165563f5ada76b1af877ea54ca83e85d65891a9f
emiyuel <108116222+emiyuel@users.noreply.github.com>
2024-08-08 23:35:25 +0900

    Title: 윤년인가, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240808/윤년인가/is-leap-year.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int y;
+
+    cin >> y;
+
+    if (!(y % 4) || !(y % 100) && !(y % 400))
+        cout << "true";
+    else
+        cout << "false";
+
+    return 0;
+}
